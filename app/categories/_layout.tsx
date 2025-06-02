9d9b8cb5be17121700ea01bc3328ca3eeb4e43a6
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-02 07:46:07 +0530

    Refactor back navigation in Category layout to use navigation prop instead of router

--- app/categories/_layout.tsx
+++ app/categories/_layout.tsx
@@ -1,20 +1,22 @@
 import { Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import { router, Stack, useRouter } from 'expo-router'
+import { Stack } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons'
 
 export default function CategoryLayout() {
-    const router = useRouter()
     return (
         <Stack>
-            <Stack.Screen name='[slug]' options={{
-                headerShown: true,
-                headerLeft: () => (
-                    <TouchableOpacity onPress={() => router.back()}>
-                        <Ionicons name='arrow-back' size={24} color='black' />
-                    </TouchableOpacity>
-                )
-            }} />
+            <Stack.Screen
+                name='[slug]'
+                options={({ navigation }) => ({
+                    headerShown: true,
+                    headerLeft: () => (
+                        <TouchableOpacity onPress={() => navigation.back()}>
+                            <Ionicons name='arrow-back' size={24} color='black' />
+                        </TouchableOpacity>
+                    )
+                })}
+            />
 
         </Stack>
     )
