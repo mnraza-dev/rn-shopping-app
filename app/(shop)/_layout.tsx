5455737db761fe75f083e5ad096c87c1d26cd23d
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-02 08:10:37 +0530

    Enhance Shop layout by adding custom tab bar styles and icons, improving visual consistency and user experience.

--- app/(shop)/_layout.tsx
+++ app/(shop)/_layout.tsx
@@ -1,16 +1,35 @@
 import { Tabs } from 'expo-router'
 import React from 'react'
+import { StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { FontAwesome } from '@expo/vector-icons'
+
+function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>['name'], color: string, size: number }) {
+    return <FontAwesome {...props} style={{ color: '#1BC464' }} size={24} />
+}
 
 const TabsLayout = () => {
     return (
-        <SafeAreaView>
-            <Tabs>
+        <SafeAreaView edges={['top']} style={styles.safeArea}>
+            <Tabs screenOptions={{
+                tabBarActiveTintColor: '#1BC464',
+                tabBarInactiveTintColor: 'gray',
+                tabBarLabelStyle: {
+                    fontSize: 12,
+
+                },
+                tabBarStyle: {
+                    paddingTop: 10,
+                    borderTopLeftRadius: 20,
+                    borderTopRightRadius: 20,
+                },
+                headerShown: false,
+            }}>
                 <Tabs.Screen name='index' options={{
                     headerShown: false,
                 }} />
                 <Tabs.Screen name='orders' options={{
-                  
+
                 }} />
             </Tabs>
         </SafeAreaView>
@@ -18,3 +37,9 @@ const TabsLayout = () => {
 }
 
 export default TabsLayout
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+
+    }
+})
