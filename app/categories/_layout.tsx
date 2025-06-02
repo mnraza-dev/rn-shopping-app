71186b73fa50d4c8c3d8a2e4ee81fb263e2ce90f
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-02 07:42:52 +0530

    Implement back navigation in Category layout by updating TouchableOpacity to use router.back()

--- app/categories/_layout.tsx
+++ app/categories/_layout.tsx
@@ -1,15 +1,16 @@
 import { Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import {  Stack } from 'expo-router'
+import { router, Stack, useRouter } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons'
 
 export default function CategoryLayout() {
+    const router = useRouter()
     return (
         <Stack>
             <Stack.Screen name='[slug]' options={{
                 headerShown: true,
                 headerLeft: () => (
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={() => router.back()}>
                         <Ionicons name='arrow-back' size={24} color='black' />
                     </TouchableOpacity>
                 )
