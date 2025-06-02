85e3cb7cf727a23a55854f9591f0747034fc42b9
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-02 07:41:18 +0530

    Enhance Category layout by enabling header visibility and adding a back button with an icon.

--- app/categories/_layout.tsx
+++ app/categories/_layout.tsx
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import { Stack } from 'expo-router'
+import {  Stack } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function CategoryLayout() {
     return (
         <Stack>
             <Stack.Screen name='[slug]' options={{
-                headerShown: false
+                headerShown: true,
+                headerLeft: () => (
+                    <TouchableOpacity>
+                        <Ionicons name='arrow-back' size={24} color='black' />
+                    </TouchableOpacity>
+                )
             }} />
-            
+
         </Stack>
     )
 }
