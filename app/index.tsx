84dcfa1435ee5c7570e20b0c6b8ca921283c9f00
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-02 07:09:11 +0530

    fitrst commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function Index() {
+    return (
+        <View>
+            <Text>Lorem ipsum  quos.
+            </Text>
+        </View>
+    )
+}
+
+export default Index
